6e9664384c72435c529f5ffbc24b49f0303e9379
Ramesh Vel <rameshvel@gmail.com>
2018-06-06 13:41:03 +0530

    Added image tool

---
+++ Inkpad-Core/Tools/WDImageTool.h
@@ -0,0 +1,12 @@
+//
+//  WDImageTool.h
+//  Inkpad
+//
+//  Created by Ramesh Vel on 06/06/18.
+//
+
+#import "WDTool.h"
+
+@interface WDImageTool : WDTool
+
+@end
---
+++ Inkpad-Core/Tools/WDImageTool.m
@@ -0,0 +1,16 @@
+//
+//  WDImageTool.m
+//  Inkpad
+//
+//  Created by Ramesh Vel on 06/06/18.
+//
+
+#import "WDImageTool.h"
+
+@implementation WDImageTool
+
+-(NSString *) iconName {
+    return @"album.png";
+}
+
+@end
